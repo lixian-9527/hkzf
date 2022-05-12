234cf665e6e4ad64eba21413b1b6a484672600aa
xian.li1 <xian.li1@pacteraedge.com>
2022-05-12 16:37:03 +0800

    git测试

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.scss';
 // 导入字体图标库的样式文件
 import './assets/fonts/iconfont.css'
 
-// 应该将组件的导入放在样式的导入的后面，从而避免全局样式覆盖问题
+// 应该将组件的导入放在样式的导入的后面，从而避免全局样式覆盖问题。
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
